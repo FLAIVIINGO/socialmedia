33b356a471b05cf92df5d6632c8c97dd294fae87
Andrew Babilonia <ababilon@gmu.edu>
2024-02-27 14:02:56 -0500

    stack navigator implemented and refactored

---
+++ screens/Home/Home.js
@@ -0,0 +1,249 @@
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  View,
+  Text,
+  FlatList,
+} from 'react-native';
+import Title from '../../components/Title/Title';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import style from './style';
+import UserStory from '../../components/UserStory/UserStory';
+import UserPost from '../../components/UserPost/UserPost';
+import {scaleFontSize} from '../../assets/styles/scaling';
+import globalStyle from '../../assets/styles/GlobalStyle';
+
+const Home = () => {
+  const userStories = [
+    {
+      firstName: 'Joseph',
+      id: 1,
+      profileImage: require('../../assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Angel',
+      id: 2,
+      profileImage: require('../../assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'White',
+      id: 3,
+      profileImage: require('../../assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Olivia',
+      id: 4,
+      profileImage: require('../../assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Nata',
+      id: 5,
+      profileImage: require('../../assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Nicolas',
+      id: 6,
+      profileImage: require('../../assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Nina',
+      id: 7,
+      profileImage: require('../../assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Nana',
+      id: 8,
+      profileImage: require('../../assets/images/default_profile.png'),
+    },
+    {
+      firstName: 'Adam',
+      id: 9,
+      profileImage: require('../../assets/images/default_profile.png'),
+    },
+  ];
+  const userPosts = [
+    {
+      firstName: 'Allison',
+      lastName: 'Becker',
+      location: 'Boston, MA',
+      likes: 1201,
+      comments: 24,
+      bookmarks: 55,
+      profileImage: require('../../assets/images/default_profile.png'),
+      image: require('../../assets/images/default_post.png'),
+      id: 1,
+    },
+    {
+      firstName: 'Jennifer',
+      lastName: 'Wilkinson',
+      location: 'Worchester, MA',
+      likes: 1301,
+      comments: 25,
+      bookmarks: 70,
+      profileImage: require('../../assets/images/default_profile.png'),
+      image: require('../../assets/images/default_post.png'),
+      id: 2,
+    },
+    {
+      firstName: 'Adam',
+      lastName: 'Spera',
+      location: 'Worchester, MA',
+      likes: 100,
+      comments: 8,
+      bookmarks: 3,
+      profileImage: require('../../assets/images/default_profile.png'),
+      image: require('../../assets/images/default_post.png'),
+      id: 3,
+    },
+    {
+      firstName: 'Andrew',
+      lastName: 'Babilonia',
+      location: 'Sterling, VA',
+      likes: 140,
+      comments: 45,
+      bookmarks: 66,
+      profileImage: require('../../assets/images/default_profile.png'),
+      image: require('../../assets/images/default_post.png'),
+      id: 4,
+    },
+    {
+      firstName: 'Nicolas',
+      lastName: 'Namoradze',
+      location: 'Berlin, Germany',
+      likes: 2000,
+      comments: 32,
+      bookmarks: 12,
+      profileImage: require('../../assets/images/default_profile.png'),
+      image: require('../../assets/images/default_post.png'),
+      id: 5,
+    },
+  ];
+
+  const userStoriesPageSize = 4;
+  const [userStoriesCurrentPage, setUserStoriesCurrentPage] = useState(1);
+  const [userStoriesRenderedData, setUserStoriesRenderedData] = useState([]);
+  const [isLoadingUserStories, setIsLoadingUserStories] = useState(false);
+
+  const userPostsPageSize = 2;
+  const [userPostsCurrentPage, setUserPostsCurrentPage] = useState(1);
+  const [userPostsRenderedData, setUserPostsRenderedData] = useState([]);
+  const [isLoadingUserPosts, setIsLoadingUserPosts] = useState(false);
+
+  const pagination = (database, currentPage, pageSize) => {
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    if (startIndex >= database.length) {
+      return [];
+    }
+    return database.slice(startIndex, endIndex);
+  };
+
+  useEffect(() => {
+    setIsLoadingUserStories(true);
+    const getInitialData = pagination(userStories, 1, userStoriesPageSize);
+    setUserStoriesRenderedData(getInitialData);
+    setIsLoadingUserStories(false);
+
+    setIsLoadingUserPosts(true);
+    const getInitialDataPosts = pagination(userPosts, 1, userPostsPageSize);
+    setUserPostsRenderedData(getInitialDataPosts);
+    setIsLoadingUserPosts(false);
+  }, []);
+
+  return (
+    <SafeAreaView style={globalStyle.backgroundWhite}>
+      <View>
+        <FlatList
+          ListHeaderComponent={
+            <>
+              <View style={style.header}>
+                <Title title={"Let's Explore"} />
+                <TouchableOpacity style={style.messageIcon}>
+                  <FontAwesomeIcon
+                    icon={faEnvelope}
+                    size={scaleFontSize(20)}
+                    color={'#898DAE'}
+                  />
+                  <View style={style.messageNumberContainer}>
+                    <Text style={style.messageNumber}>2</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+              <View style={style.userStoryContainer}>
+                <FlatList
+                  onEndReachedThreshold={0.5}
+                  onEndReached={() => {
+                    if (isLoadingUserStories) {
+                      return;
+                    }
+                    setIsLoadingUserStories(true);
+                    const contentToAppend = pagination(
+                      userStories,
+                      userStoriesCurrentPage + 1,
+                      userStoriesPageSize,
+                    );
+                    if (contentToAppend.length > 0) {
+                      setUserStoriesCurrentPage(userStoriesCurrentPage + 1);
+                      setUserStoriesRenderedData(prev => [
+                        ...prev,
+                        ...contentToAppend,
+                      ]);
+                    }
+                    setIsLoadingUserStories(false);
+                  }}
+                  showsHorizontalScrollIndicator={false}
+                  horizontal={true}
+                  data={userStoriesRenderedData}
+                  renderItem={({item}) => (
+                    <UserStory
+                      key={'userStory' + item.id}
+                      firstName={item.firstName}
+                      profileImage={item.profileImage}
+                    />
+                  )}
+                />
+              </View>
+            </>
+          }
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+            if (isLoadingUserPosts) {
+              return;
+            }
+            setIsLoadingUserPosts(true);
+            const contentToAppend = pagination(
+              userPosts,
+              userPostsCurrentPage + 1,
+              userPostsPageSize,
+            );
+            if (contentToAppend.length > 0) {
+              setUserPostsCurrentPage(userPostsCurrentPage + 1);
+              setUserPostsRenderedData(prev => [...prev, ...contentToAppend]);
+            }
+            setIsLoadingUserPosts(false);
+          }}
+          data={userPostsRenderedData}
+          showsVerticalScrollIndicator={false}
+          renderItem={({item}) => (
+            <View style={style.userPostContainer}>
+              <UserPost
+                firstName={item.firstName}
+                lastName={item.lastName}
+                image={item.image}
+                likes={item.likes}
+                comments={item.comments}
+                bookmarks={item.bookmarks}
+                profileImage={item.profileImage}
+                location={item.location}
+              />
+            </View>
+          )}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
